07c60997138b76398d1853a509191983e17a28fa
Moin Khan <khanmoin3757@gmail.com>
2025-01-08 22:04:31 +0530

    Made it Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 const App = () => {
   return (
     <div
-      className="w-full h-screen flex justify-center items-center relative"
+      className="w-[101%] h-[101vh] flex justify-center items-center relative overflow-hidden"
       style={{
         background: "linear-gradient(to bottom, #031F36, #0A599C)",
       }}
     >
-      <img className="absolute top-0 w-full sm:w-[80%] h-full object-cover" src="./images/bg.png" alt="" />
-      <img className="p-4" src="./images/full_logo.png" alt="" />
-      <img className="absolute w-1/2 sm:w-auto left-[52%] top-[54%] sm:left-[53%] sm:top-[58%]" src="./images/abc.png" alt="" />
+      <img
+        className="absolute top-0 w-full h-full object-cover"
+        src="./images/bg.png"
+        alt="background"
+      />
+      <img
+        className="p-4 sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%]"
+        src="./images/full_logo.png"
+        alt="logo"
+      />
+      <img
+        className="absolute w-[60%] sm:w-[50%] md:w-[40%] lg:w-[30%] left-[70%] transform -translate-x-1/2 top-[55%] sm:top-[56%] md:top-[56%]"
+        src="./images/abc.png"
+        alt="image"
+      />
     </div>
   );
 };
